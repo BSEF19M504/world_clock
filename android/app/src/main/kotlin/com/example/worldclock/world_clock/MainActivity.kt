999345c9efabfa59654d42a8acf7c3ef783ea2c4
BSEF19M504 <bsef19m504@pucit.edu.pk>
2023-01-04 15:41:58 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/worldclock/world_clock/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.worldclock.world_clock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: ClockApp(),
+  ));
+}
+
+class ClockApp extends StatelessWidget {
+  const ClockApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
