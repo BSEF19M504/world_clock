09fe321d149eb1ac313bcc2dbf753c6e10e20a21
BSEF19M504 <bsef19m504@pucit.edu.pk>
2023-01-07 00:29:00 +0500

    Implemented World Time API for Asia/Karachi timezone

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -8,6 +10,27 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getTime() async {
+    Response response = await get(Uri.parse("https://worldtimeapi.org/api/timezone/Asia/Karachi"));
+    Map data = jsonDecode(response.body);
+    //print(data);
+    String datetime = data["datetime"];
+    String offset = data["utc_offset"];
+    print(datetime);
+    print(offset);
+
+    // Create a DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset.substring(1,3))));
+    print(now);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
