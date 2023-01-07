ff70c85fce2f12b57c6c5032aa17a09dcc88a5f9
BSEF19M504 <bsef19m504@pucit.edu.pk>
2023-01-08 00:00:00 +0500

    Implemeneted time class logic

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -2,4 +2,24 @@ import 'package:http/http.dart';
 import 'dart:convert';
 
 class WorldTime {
+  String location;
+  String time = "";
+  String flag;
+  String url;
+
+  WorldTime({required this.location, required this.flag, required this.url});
+
+  Future<void> getTime() async {
+    Response response = await get(Uri.parse("https://worldtimeapi.org/api/timezone/$url"));
+    Map data = jsonDecode(response.body);
+    //print(data);
+    String datetime = data["datetime"];
+    String offset = data["utc_offset"];
+
+    // Create a DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset.substring(1,3))));
+
+    time = now.toString();
+  }
 }
