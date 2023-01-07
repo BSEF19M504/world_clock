262c7a95691f29a4e53dd93ed588ef6b4e502025
BSEF19M504 <bsef19m504@pucit.edu.pk>
2023-01-08 00:00:00 +0500

    Implemented time class logic

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_clock/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -11,25 +10,14 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async {
-    Response response = await get(Uri.parse("https://worldtimeapi.org/api/timezone/Asia/Karachi"));
-    Map data = jsonDecode(response.body);
-    //print(data);
-    String datetime = data["datetime"];
-    String offset = data["utc_offset"];
-    print(datetime);
-    print(offset);
-
-    // Create a DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset.substring(1,3))));
-    print(now);
+  void setupWorldTime() async{
+    WorldTime instance = WorldTime(location: "Karachi", flag: "pakistan.png", url: "Asia/Karachi");
+    await instance.getTime();
   }
-
   @override
   void initState() {
     super.initState();
-    getTime();
+    setupWorldTime();
   }
   @override
   Widget build(BuildContext context) {
