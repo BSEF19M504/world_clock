29dbae6eb9ba49e68219a7f405fdabf55fde0c02
BSEF19M504 <bsef19m504@pucit.edu.pk>
2023-01-08 16:00:47 +0500

    Changed dat format and displayed it on home widget

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   String location;
@@ -21,8 +22,9 @@ class WorldTime {
       DateTime now = DateTime.parse(datetime);
       now = now.add(Duration(hours: int.parse(offset.substring(1,3))));
 
-      time = now.toString();
+      time = DateFormat.jm().format(now);
     } catch (e) {
+      print("Caught error: $e");
       time = "Could not get time data";
     }
   }
