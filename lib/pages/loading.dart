d9cdbd9dd89aba6470cd4b77a78d333ae17ca45b
BSEF19M504 <bsef19m504@pucit.edu.pk>
2023-01-08 19:46:39 +0500

    Added loading spinner

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:world_clock/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -13,26 +14,39 @@ class _LoadingState extends State<Loading> {
   late NavigatorState navigator = NavigatorState();
   void setupWorldTime() async{
     WorldTime instance = WorldTime(location: "Karachi", flag: "pakistan.png", url: "Asia/Karachi");
-    await instance.getTime();
+    while(true){
+    //for(int i = 0; i < 5; i++){
+      await instance.getTime();
+      if (instance.time != "Could not get time data") {
+        break;
+      }
+    }
     navigator.pushReplacementNamed("/home", arguments: {
       "location": instance.location,
       "flag": instance.flag,
       "time": instance.time
     });
   }
+
   @override
   void initState() {
     super.initState();
     navigator =Navigator.of(context);
     setupWorldTime();
   }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50.0),
-        child: Text("loading"),
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: SpinKitCircle(
+          color: Colors.white,
+          duration: Duration(milliseconds: 1200),
+          size: 80.0,
+        ),
       ),
     );
   }
 }
+
